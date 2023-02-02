4a4a66043c4c5a08f9c35ff2d0dccb3cb3228b97
augustinebest <augustusbest00@gmail.com>
2023-02-02 13:15:22 +0100

    Added reverse string Algo

---
+++ 01-02-2023/reverseString.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+const reverseString = (s) => {
+  let left = 0,
+    right = s.length - 1;
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+  return s;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
