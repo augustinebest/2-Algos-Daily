34c42cf01a65d8a988343f1a37256dba9adaf19e
augustinebest <augustusbest00@gmail.com>
2023-02-02 13:16:09 +0100

    Added first unique char Algo

---
+++ 02-02-2023/firstUniqChar.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var firstUniqChar = function (s) {
+  const map = {};
+
+  for (const char of s) map[char] = (map[char] += 1) || 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === 1) return i;
+  }
+
+  return -1;
+};
+
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("loveleetcode"));
+console.log(firstUniqChar("aabb"));
+console.log(firstUniqChar("z"));
