dc23b8bb86906201b1618e691949611bfffc3732
augustinebest <augustusbest00@gmail.com>
2023-02-02 13:15:52 +0100

    Added canConstruct Algo

---
+++ 02-02-2023/canConstruct.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  for (const char of magazine) {
+    ransomNote = ransomNote.replace(char, "");
+  }
+
+  if (!ransomNote) return true;
+  return false;
+};
+
+console.log(canConstruct("a", "b"));
+console.log(canConstruct("aa", "aab"));
